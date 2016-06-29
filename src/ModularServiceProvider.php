2676bb5453ec190edb109947c12297cea15b4be7
Daison Carino <daison12006013@gmail.com>
2016-06-29 08:15:41 +0000

    changing module:auth to only allow laravel >= 5.2 versions

--- src/ModularServiceProvider.php
+++ src/ModularServiceProvider.php
@@ -2,6 +2,7 @@
 namespace LaraLibs\Modular;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Foundation\Application as APP;
 
 class ModularServiceProvider extends ServiceProvider
 {
@@ -12,14 +13,19 @@ class ModularServiceProvider extends ServiceProvider
 
     public function register()
     {
-        $this->commands([
-            Commands\Module\Auth::class,
+        $commands = [
             // Commands\Module\Console::class,
             // Commands\Module\Controller::class,
             // Commands\Module\Middleware::class,
             // Commands\Module\Model::class,
             // Commands\Module\Provider::class,
             Commands\Make\Module::class,
-        ]);
+        ];
+
+        if ((float) APP::VERSION >= (float) 5.2) {
+            $commands[] = Commands\Module\Auth::class;
+        }
+
+        $this->commands($commands);
     }
 }
